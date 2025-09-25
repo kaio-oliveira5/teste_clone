60c518bae5e08f74ab0e951b5de857b0fe32d4db
kaio <kaiovinicios05@gmail.com>
2025-09-25 18:54:00 -0300

    teste clone

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+function html() {
+    return gulp.src('./*.html')
+    .pipe(gulp.dest('./dist'));
+}
+
+exports.default =gulp.parallel(styles, images, html);
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./*.html', gulp.parallel(html));
+};
